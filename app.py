b60079414e6a495e2ad16b7928efa8906c3b0e03
jerryec26 <jespinozac26@gmail.com>
2025-07-30 23:46:47 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect
 from registro import registrar_datos
+import os
 
 app = Flask(__name__)
 
@@ -22,4 +23,7 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Puerto asignado por Render
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
